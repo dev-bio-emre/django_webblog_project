e528d9ff2d5234155648e9298da94e43cf584144
Emre Engin <emreenginn@gmail.com>
2026-02-15 18:38:28 +0300

    Proje Dosyaları Eklendi

---
+++ README.md
@@ -0,0 +1,6 @@
+## Django projesi başlatma
+## Başlatılan projeyle etkileşime girmek için kullanılan komutlar
+## Varsayılan olarak gelen proje dosyalarının işlevleri
+## App oluşturma
+## Veri Tabanı İşlemleri
+## Admin Panel
---
+++ blog/posts/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+    email = models.EmailField()
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=50) # kısa metinler için CharField kullanılır, max_length parametresi zorunludur
+    content = models.TextField() # daha uzun metinler için TextField kullanılır
+    created = models.DateTimeField(auto_now_add=True) # otomatik olarak oluşturulma tarihini ekler.
+    updated = models.DateTimeField(auto_now=True) # her kaydedildiğinde güncellenme tarihini ekler.
+    is_published = models.BooleanField(default=False) # varsayılan olarak False, yani yayınlanmamış olarak başlar.
+    
+    
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL) # bir postun bir yazarı olabilir, on_delete=models.SET_NULL ile yazar silindiğinde postun author alanı null olur.
+    tags = models.ManyToManyField(Tag, blank=True) # bir postun birden fazla tagi olabilir, blank=True ile boş bırakılabilir hale getiriyoruz.
+
